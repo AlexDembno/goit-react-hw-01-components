d0235a1ab2bfd42ed62c4fa7bb848f7b37aed097
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2023-01-16 21:01:59 +0100

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,14 @@
 import styles from './Profile.module.css';
 import PropTypes from 'prop-types';
 
-function Profile({ username, tag, location, avatar, stats }) {
-  const keysStats = Object.keys(stats);
+function Profile({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) {
+  const keysStats = Object.keys({ followers, views, likes });
   return (
     <div className={styles.profile}>
       <div className={styles.description}>
@@ -15,15 +21,15 @@ function Profile({ username, tag, location, avatar, stats }) {
       <ul className={styles.stats}>
         <li className={styles.item}>
           <span className={styles.label}>{keysStats[0]}</span>
-          <span className={styles.quantity}>{stats.followers}</span>
+          <span className={styles.quantity}>{followers}</span>
         </li>
         <li className={styles.item}>
           <span className={styles.label}>{keysStats[1]}</span>
-          <span className={styles.quantity}>{stats.views}</span>
+          <span className={styles.quantity}>{views}</span>
         </li>
         <li className={styles.item}>
           <span className={styles.label}>{keysStats[2]}</span>
-          <span className={styles.quantity}>{stats.likes}</span>
+          <span className={styles.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
@@ -31,7 +37,6 @@ function Profile({ username, tag, location, avatar, stats }) {
 }
 
 export default Profile;
-
 
 Profile.propTypes = {
   username: PropTypes.string,
